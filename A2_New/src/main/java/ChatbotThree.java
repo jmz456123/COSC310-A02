22420b3bbb301285fb4ff738ca207d22ce0214c6
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2020-04-01 11:41:44 +0800

    Documentation for the running file

--- A2_New/src/main/java/ChatbotThree.java
+++ A2_New/src/main/java/ChatbotThree.java
@@ -1,5 +1,4 @@
 
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -23,7 +22,7 @@ public class ChatbotThree extends JFrame {
 	private static JTextArea chatArea = new JTextArea();
 	private static JTextField chatBox = new JTextField();
 
-	public ChatbotThree() {
+	public ChatbotThree() {// default constructor setting up the GUI
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 		setResizable(false);
@@ -32,47 +31,34 @@ public class ChatbotThree extends JFrame {
 		setTitle("ChatBot");
 		add(chatArea);
 		add(chatBox);
+		//ignore the above, use the four below to manipulate GUI
 		chatArea.setSize(470, 500);
 		chatArea.setLocation(10, 10);
 		chatBox.setSize(470, 30);
 		chatBox.setLocation(10, 520);
 	}
 
-	private static void bot1(String text) {
-		chatArea.append("-" + "\n");
-	}
-
 	public static void main(String[] args) {
 		try {
 			// AddAiml.main(null);//invoke methods in addaiml.java
-
 			String resourcesPath = getResourcesPath();
 			// System.out.println(resourcesPath);// print resourcePath
 
 			MagicBooleans.trace_mode = TRACE_MODE;
 			bot = new Bot("super", resourcesPath);
 			chatSession = new Chat(bot);
-			bot.brain.nodeStats();
-			String textLine = "";
+			bot.brain.nodeStats();//this sets up the chatbot for conversation
 
 			new ChatbotThree();
 			chatBox.addActionListener(new ActionListener() {
+				//similar to a while loop, this method keeps asking user for input
 				public void actionPerformed(ActionEvent arg0) {
-					String text = chatBox.getText();
-					chatArea.append("YOU ->:" + text + "\n");
-					chatBox.setText("");
-					// chatArea.append(chatString(bot1, chatSession, text));
-					answerUser(text);
+					String text = chatBox.getText();//similar to in.nextLine() from Scanner
+					chatArea.append("YOU ->:" + text + "\n");//chatArea.append prints thing on the screen
+					chatBox.setText("");//flush text away
+					answerUser(text);//Ab.jar process input and prints results
 				}
 			});
-
-			//
-			// while (true) {
-			// System.out.print("Human : ");
-			// textLine = IOUtils.readInputTextLine();
-			// answerUser(textLine);
-			//
-			// }
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -86,17 +72,18 @@ public class ChatbotThree extends JFrame {
 		} else if (textLine.equals("wq")) {
 			bot.writeQuit();
 			System.exit(0);
-		} else {
+		} else {//if the input is valid
 			String request = textLine;
 			if (MagicBooleans.trace_mode)
 				chatArea.append("STATE=" + request + ":THAT=" + ((History) chatSession.thatHistory.get(0)).get(0)
 						+ ":TOPIC=" + chatSession.predicates.get("topic") + "\n");
+			//give feedback for user inputs
 			String response = chatSession.multisentenceRespond(request);
 			while (response.contains("&lt;"))
 				response = response.replace("&lt;", "<");
 			while (response.contains("&gt;"))
 				response = response.replace("&gt;", ">");
-			chatArea.append("Robot : " + response + "\n");
+			chatArea.append("Robot : " + response + "\n");//print results on GUI
 		}
 	}
 
